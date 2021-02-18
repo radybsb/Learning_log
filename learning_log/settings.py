3bfa30480ce3ddba58fee0cf346179680e287ff2
radybsb <radybsb@gmail.com>
2021-02-18 02:09:40 -0300

    Ready for deployment to heroku.

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -138,23 +138,23 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 # Heroku settings
-if os.getcwd() == '/app':
-    DATABASES = {
-        'default': dj_database_url.config(default='postgres://localhost')
-    }
+# if os.getcwd() == '/app':
+DATABASES = {
+    'default': dj_database_url.config(default='postgres://localhost')
+}
 
-    # Honor the 'X-Forwarded-Proto' header for request.is_secure().
-    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+# Honor the 'X-Forwarded-Proto' header for request.is_secure().
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-    # Allow all host headers.
-    ALLOWED_HOSTS = ['*']
+# Allow all host headers.
+ALLOWED_HOSTS = ['*']
 
-    # Static asset configuration
-    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    STATIC_URL = '/static/'
-    STATICFILES_DIRS = (
-        os.path.join(BASE_DIR, 'static'),
-    )
+# Static asset configuration
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
